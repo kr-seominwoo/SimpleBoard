eb6ec5de23abfbd4171a8838d35b0603eab293ac
DESKTOP-24FN8J6\seo <tjalsdnss@gmail.com>
2022-08-06 16:29:41 +0900

    index 페이지 컨트롤러 및 기본 index.jsp 추가

---
+++ boardPrj/src/main/java/com/board/web/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.board.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	@RequestMapping(value= {"", "index"})
+	public String index() {
+		return "index";
+	}
+}
