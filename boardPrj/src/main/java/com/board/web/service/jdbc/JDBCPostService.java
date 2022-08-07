d9ce9165c5dcd114c6b3c39218c0317bc6071aca
DESKTOP-24FN8J6\seo <tjalsdnss@gmail.com>
2022-08-07 20:16:51 +0900

    오라클 db로부터 데이터를 조회 및 업데이트하는 서비스 인터페이스를 구현하는 JDBCPostService클래스 구현. 현재 글 등록 조회 및 댓글 등록 조회 기능 구현

---
+++ boardPrj/src/main/java/com/board/web/service/jdbc/JDBCPostService.java
@@ -0,0 +1,189 @@
+package com.board.web.service.jdbc;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.board.web.entity.Comment;
+import com.board.web.entity.Post;
+import com.board.web.entity.PostView;
+import com.board.web.security.SHA256;
+import com.board.web.service.PostService;
+
+@Service
+public class JDBCPostService implements PostService {
+	@Autowired
+	DataSource dataSource;
+
+	@Override
+	public int registPost(String writerId, String title, String content, String password, String hashtags,
+			int hashtagsCount) {
+		int result = 0;
+
+		String sql = "INSERT INTO POST(USER_ID, PASSWORD, TITLE, CONTENT, HASHTAGS, HASHTAGS_COUNT, POST_NUMBER) VALUES(?,?,?,?,?,?, POST_NUMBER_SEQ.NEXTVAL)";
+		SHA256 sha256 = new SHA256();
+		String encryptedPassword = "";
+		try {
+			encryptedPassword = sha256.encrypt(password);
+		} catch (NoSuchAlgorithmException e1) {
+			e1.printStackTrace();
+			return result;
+		}
+
+		try {
+			Connection con = this.dataSource.getConnection();
+			PreparedStatement st = con.prepareStatement(sql);
+
+			st.setString(1, writerId);
+			st.setString(2, encryptedPassword);
+			st.setString(3, title);
+			st.setString(4, content);
+			st.setString(5, hashtags);
+			st.setInt(6, hashtagsCount);
+
+			result = st.executeUpdate();
+
+			st.close();
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+	@Override
+	public List<PostView> getPostViewList() {
+		List<PostView> list = new ArrayList<>();
+
+		String sql = "SELECT * FROM POST P ORDER BY P.POST_DATE DESC";
+
+		try {
+			Connection con = this.dataSource.getConnection();
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+
+			while (rs.next()) {
+				int postNumber = rs.getInt("POST_NUMBER");
+				String title = rs.getString("TITLE");
+				String writerId = rs.getString("USER_ID");
+				Date postDate = rs.getDate("POST_DATE");
+				int commentCount = 0;
+				int hit = rs.getInt("HIT");
+				int like = rs.getInt("LIKE");
+
+				PostView postView = new PostView(postNumber, title, writerId, postDate, commentCount, hit, like);
+				list.add(postView);
+			}
+
+			st.close();
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return list;
+	}
+
+	@Override
+	public Post getPost(int postNumber) {
+		Post post = null;
+		List<Comment> commentList = new ArrayList<>();
+
+		String sqlPost = "SELECT * FROM POST P WHERE P.POST_NUMBER = " + postNumber;
+		String sqlComment = "SELECT * FROM \"COMMENT\" C WHERE C.POST_NUMBER = " + postNumber + " ORDER BY C.COMMENT_DATE DESC";
+
+		try {
+			Connection con = this.dataSource.getConnection();
+			Statement stPost = con.createStatement();
+			ResultSet rs = stPost.executeQuery(sqlPost);
+
+			if (rs.next()) {
+				String writerId = rs.getString("USER_ID");
+				String title = rs.getString("TITLE");
+				String content = rs.getString("CONTENT");
+				Date postDate = rs.getDate("POST_DATE");
+				Date modifyDate = rs.getDate("MODIFY_DATE");
+				String hashtags = rs.getString("HASHTAGS");
+				int hit = rs.getInt("HIT");
+				int like = rs.getInt("LIKE");
+
+				List<String> hashtagList = new ArrayList<>();
+				StringTokenizer tokenizer = new StringTokenizer(hashtags, ",");
+				while (tokenizer.hasMoreTokens()) {
+					hashtagList.add(tokenizer.nextToken());
+				}
+
+				post = new Post(writerId, title, content, postDate, modifyDate, hashtagList, like, hit, postNumber,
+						commentList);
+			}
+			
+			Statement stComment = con.createStatement();
+			rs = stComment.executeQuery(sqlComment);
+			
+			while (rs.next()) {
+				String writerId = rs.getString("USER_ID");
+				String content = rs.getString("CONTENT");
+				Date commentDate = rs.getDate("COMMENT_DATE");
+				
+				Comment comment = new Comment(writerId, content, commentDate, postNumber);
+				commentList.add(comment);
+			}
+			
+
+			stPost.close();
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return post;
+	}
+
+	@Override
+	public int registComment(String writerId, String content, String password, int postNumber) {
+		int result = 0;
+
+		String sql = "INSERT INTO \"COMMENT\" (USER_ID, PASSWORD, CONTENT, POST_NUMBER) VALUES(?,?,?,?)";
+		SHA256 sha256 = new SHA256();
+		String encryptedPassword = "";
+		try {
+			encryptedPassword = sha256.encrypt(password);
+		} catch (NoSuchAlgorithmException e1) {
+			e1.printStackTrace();
+			return result;
+		}
+
+		try {
+			Connection con = this.dataSource.getConnection();
+			PreparedStatement st = con.prepareStatement(sql);
+
+			st.setString(1, writerId);
+			st.setString(2, encryptedPassword);
+			st.setString(3, content);
+			st.setInt(4, postNumber);
+
+			result = st.executeUpdate();
+
+			st.close();
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+}
