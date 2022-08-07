57fbe666e56bc0e20f18009419a61bdb97f81993
DESKTOP-24FN8J6\seo <tjalsdnss@gmail.com>
2022-08-07 20:19:08 +0900

    controller에서 글 등록, 글 목록 조회, 글 세부내용 조회, 댓글 등록 서비스기능 사용하도록 구현

--- boardPrj/src/main/java/com/board/web/controller/post/PostController.java
+++ boardPrj/src/main/java/com/board/web/controller/post/PostController.java
@@ -1,46 +1,86 @@
 package com.board.web.controller.post;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.board.web.entity.Post;
+import com.board.web.entity.PostView;
+import com.board.web.service.jdbc.JDBCPostService;
+
 @Controller
 @RequestMapping("/post/")
 public class PostController {
-	@GetMapping("list") 
-	public String list() {
+	@Autowired
+	private JDBCPostService service;
+
+	@GetMapping("list")
+	public String list(Model model) {
+		List<PostView> list = service.getPostViewList();
+		model.addAttribute("list", list);
 		return "/post/list";
 	}
-	
+
 	@GetMapping("write")
 	public String write() {
 		return "/post/write";
 	}
-	
+
 	@PostMapping("regist")
-	public String regist() {
+	public String regist(String writerId, String title, String content, String password, String[] hashTag) {
+		String hashTags = "";
+		int hashTagsCount = 0;
+		if (hashTag != null) {
+			StringBuilder builder = new StringBuilder();
+			for (int idx = 0; idx < hashTag.length; ++idx) {
+				builder.append(hashTag[idx]);
+
+				if (idx != hashTag.length - 1) {
+					builder.append(",");
+				}
+			}
+
+			hashTagsCount = hashTag.length;
+			hashTags = builder.toString();
+		}
+
+		this.service.registPost(writerId, title, content, password, hashTags, hashTagsCount);
 		return "redirect:/post/list";
-	}	
-	
+	}
+
 	@GetMapping("detail")
-	public String detail() {
+	public String detail(int postNumber, Model model) {
+		Post post = this.service.getPost(postNumber);
+		model.addAttribute("post", post);
 		return "/post/detail";
 	}
-	
-	@GetMapping("edit")
-	public String edit() {
+
+	@PostMapping("edit")
+	public String edit(String password, int postNumber) {
 		return "/post/edit";
 	}
-	
+
 	@PostMapping("update")
-	public String update() {
+	public String update(String title, String content, String[] hashTag) {
 		return "redirect:/post/detail";
 	}
-	
+
 	@PostMapping("delete")
-	public String delete() {
+	public String delete(int postNumber) {
 		return "redirect:/post/list";
+	}
+	
+	@PostMapping("registComment")
+	public String registComment(String writerId, String content, String password, int postNumber) {
+		System.out.println("postNumber : " + postNumber);
+		this.service.registComment(writerId, content, password, postNumber);
+		
+		return "redirect:/post/detail?postNumber=" + postNumber;
 	}
 }
