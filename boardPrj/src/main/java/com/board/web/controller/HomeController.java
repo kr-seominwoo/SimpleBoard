80ac0be138ac758b4c66ea30ebfcbd4042aac1d2
DESKTOP-24FN8J6\seo <tjalsdnss@gmail.com>
2022-08-07 13:17:58 +0900

    각 Controller 메서드들의 RequesMapping을 crud에 맞게 어노테이션 변경

--- boardPrj/src/main/java/com/board/web/controller/HomeController.java
+++ boardPrj/src/main/java/com/board/web/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.board.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 	
-	@RequestMapping(value= {"", "index"})
+	@GetMapping(value= {"", "index"})
 	public String index() {
 		return "index";
 	}
--- boardPrj/src/main/java/com/board/web/controller/post/PostController.java
+++ boardPrj/src/main/java/com/board/web/controller/post/PostController.java
@@ -1,42 +1,45 @@
 package com.board.web.controller.post;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/post/")
 public class PostController {
-	@RequestMapping("list") 
+	@GetMapping("list") 
 	public String list() {
 		return "/post/list";
 	}
 	
-	@RequestMapping("write")
+	@GetMapping("write")
 	public String write() {
 		return "/post/write";
 	}
 	
-	@RequestMapping("regist")
+	@PostMapping("regist")
 	public String regist() {
 		return "redirect:/post/list";
 	}	
 	
-	@RequestMapping("detail")
+	@GetMapping("detail")
 	public String detail() {
 		return "/post/detail";
 	}
 	
-	@RequestMapping("edit")
+	@GetMapping("edit")
 	public String edit() {
 		return "/post/edit";
 	}
 	
-	@RequestMapping("update")
+	@PostMapping("update")
 	public String update() {
 		return "redirect:/post/detail";
 	}
 	
-	@RequestMapping("delete")
+	@PostMapping("delete")
 	public String delete() {
 		return "redirect:/post/list";
 	}
